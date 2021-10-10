5ff3cd468d1a28ef4e7a62cd8ca83a9743cd4768
gparada <gparada@carltonone.com>
2021-10-09 20:16:59 -0700

    using composition api

---
+++ src/components/PostsList.vue
@@ -0,0 +1,24 @@
+<template>
+  <h2>Post lists:</h2>
+  <div class="postlists">
+    <div v-for="post in posts" :key="post.id" class="">
+      <SinglePost :post="post"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import SinglePost from './SinglePost.vue'
+export default {
+  props: ['posts'],
+  components: { SinglePost },
+  setup(props){
+    
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home!</h1>
+    <PostList :posts="posts"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import {ref} from 'vue'
+import PostList from '../components/PostsList.vue'
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  components: { PostList},
+  setup(){
+    const posts = ref([
+      {title: 'Greeting', body: '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae doloremque adipisci animi laudantium nostrum tenetur alias. Beatae minima laudantium doloribus. Ex voluptatum earum totam soluta, illum, architecto similique alias laudantium iste officia consequatur aperiam exercitationem labore eum quo aspernatur mollitia beatae. Atque laudantium eius ad? Eaque facere repudiandae, vitae consectetur dolorum, ratione vel ad beatae id at quos consequuntur dicta molestias hic. Odit, placeat error delectus quia laudantium, ipsa omnis praesentium quisquam dolorem quas nesciunt aspernatur rem nobis a sunt ipsam laboriosam. Veniam ratione reprehenderit quidem expedita nam natus, maxime explicabo dolor sapiente. Culpa, tempora dignissimos facere perspiciatis illum eligendi distinctio reprehenderit laborum labore ipsum qui non suscipit minima nostrum libero quod dolorem maxime nisi nihil sequi quaerat? Cupiditate illo, non necessitatibus excepturi, doloribus nostrum dolore voluptate officia perferendis eaque quam sed. Cupiditate ex nesciunt, aliquid debitis esse itaque minima incidunt sapiente voluptas? Deleniti, amet. Harum quia placeat possimus aspernatur tenetur dolor repudiandae consequatur impedit autem. Corrupti, autem harum? At, ad aliquid nihil dolores libero eum esse facere quidem nam odit. Tempore labore libero accusantium tenetur consequatur, voluptatem optio, velit quo, ad impedit culpa. Odio beatae adipisci harum repellat doloremque enim a facere. Deleniti, perspiciatis quo explicabo dolor harum debitis.', id:1 },
+      {title: 'A body', body: 'Hi this is my body!', id: 2}
+    ]) 
+    return { posts }
   }
 }
 </script>
+
+<style>
+
+</style>
